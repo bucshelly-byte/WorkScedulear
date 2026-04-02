229ff5ac9bd5358a312d8c474a2c98f96f7d4ac7
bucshelly-byte <bucshelly@gmail.com>
2026-04-02 18:03:41 +0300

    Update app.js

--- static/app.js
+++ static/app.js
@@ -1,1 +1,13 @@
+const KEY = "ShellySecureKey_9843_2024_XYZ";
 
+async function navigate(page, params = "") {
+    const res = await fetch(`/pages/${page}.html`);
+    const html = await res.text();
+    document.getElementById("app").innerHTML = html;
+
+    if (window[`init_${page}`]) {
+        window[`init_${page}`](params);
+    }
+}
+
+window.onload = () => navigate("home");
