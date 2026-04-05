b64f91919ad9d583912bde99e6044b56afafcdb8
bucshelly-byte <bucshelly@gmail.com>
2026-04-05 16:20:55 +0300

    Update server.py

--- server.py
+++ server.py
@@ -358,4 +358,4 @@ def delete_visit(id):
 # RUN
 # ----------------------------------------------------
 if __name__ == "__main__":
-app.run(host="0.0.0.0", port=8000, debug=False, use_reloader=False)
+    app.run(host="0.0.0.0", port=8000, debug=False, use_reloader=False)
